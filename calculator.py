1f7c4e7d6d34d15c2882589acb0551b4ed83c8b4
MONIKAPRAJAPAT <79514533+MONIKAPRAJAPAT@users.noreply.github.com>
2023-04-18 11:55:36 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,21 @@
+print('''
++ADD
+-SUB
+*MUL
+/DIVIDE
+''')
+num1=int(input("Enter First Number"))
+num2=int(input("Enter Secound Number"))
+opr=input("Please Enter Operator + - * /")
+if(opr=="+"):
+    c=num1+num2
+    print(c)
+elif(opr=="-"):
+    c=num1-num2
+    print(c)
+elif(opr=="*")
+    c=num1*num2
+    print(c)
+elif(opr=="/")
+    c=num1/num2
+    print(c)
